da9bafaf9fd659688ef344225548c57f2990e518
lanlan2017 <18251956727@163.com>
2019-08-15 17:20:10 +0800

    增加一些特殊字符的读音

--- src/replace/MDCodeReplace.java
+++ src/replace/MDCodeReplace.java
@@ -1,110 +1,105 @@
 package replace;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import regex.RegexMath;
 import tools.io.properties.ContainSpecialWordsProperties;
 import tools.io.properties.SpecialWordsProperties;
 
-public class MDCodeReplace
-{
-	/**
-	 * 处理包含这些单词的情况.
-	 * 
-	 * @param matcherStr
-	 * @return
-	 */
-	public static String replaceContainSpecialWords(String matcherStr)
-	{
-		matcherStr = ContainSpecialWordsProperties
-				.repalceByProperties(matcherStr);
-		return matcherStr;
-	}
-	/**
-	 * 替换特殊字符.
-	 * 
-	 * @param matcherStr.
-	 * @return
-	 */
-	public static String replaceSpecialChars(String matcherStr)
-	{
-		matcherStr = matcherStr.replace("=", "等于");
-		matcherStr = matcherStr.replace("-", "杠");
-		matcherStr = matcherStr.replace("&", "单与符号");
-		matcherStr = matcherStr.replace("?", "问号");
-		matcherStr = matcherStr.replace("*", "星号");
-		matcherStr = matcherStr.replace("/", "斜杠");
-		matcherStr = matcherStr.replace("\\", "反斜杠");
-		matcherStr = matcherStr.replace(":", "冒号");
-		matcherStr = matcherStr.replace("$", "美元");
-		matcherStr = matcherStr.replace("#", "井号");
-		matcherStr = matcherStr.replace(".", "点");
-		matcherStr = matcherStr.replace("|", "竖杠");
-		matcherStr = matcherStr.replace("!", "感叹号");
-//		matcherStr = matcherStr.replace(",", "逗号");
-		return matcherStr;
-	}
-	/**
-	 * 处理匹配特定正则表达式的情况.
-	 * 
-	 * @param matcherStr
-	 * @return
-	 */
-	public static String replaceMatcher(String matcherStr)
-	{
-		// HTML代码
-		Pattern htmlTagPattern = Pattern.compile("<(.+?)/?>");
-		Matcher htmlTagMatcher = htmlTagPattern.matcher(matcherStr);
-		// 如果是负数的情况
-//		if (matcherStr.matches("-\\d+"))
-		if (matcherStr.matches(RegexMath.negativeNumber))
-		{
-			// System.out.println("匹配到负数: " + matcherStr);
-			matcherStr = matcherStr.replace("-", "负");
-		}
-		// 朗读全大写字符串
-		matcherStr = replaceStringAllUpperCase(matcherStr);
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-		// 如果是html标签
-		if (htmlTagMatcher.matches())
-		{
-			// 移除html标签的`<`和`/?>`
-			matcherStr = htmlTagMatcher.replaceAll("$1");
-		}
-		return matcherStr;
-	}
-	/**
-	 * 朗读全大写的字符串.
-	 * 
-	 * @param matcherStr
-	 *            全部大写的字符串.
-	 * @return 各个大写分开的字符串.
-	 */
-	private static String replaceStringAllUpperCase(String matcherStr)
-	{
-		Pattern pattern = Pattern.compile("[A-Z]+");
-		Matcher matcher = pattern.matcher(matcherStr);
-		if (matcher.matches())
-		{
-			StringBuffer sbBuffer = new StringBuffer(matcherStr.length() * 2);
-			for (int i = 0, length = matcherStr.length(); i < length; i++)
-			{
-				sbBuffer.append(matcherStr.charAt(i) + " ");
-			}
-			return sbBuffer.toString();
-		}
-		return matcherStr;
-	}
-	/**
-	 * 替换容易读错的单词.
-	 * 
-	 * @param matcherStr
-	 *            容易读错的单词.
-	 * @return
-	 */
-	public static String replaceSpecialWords(String matcherStr)
-	{
-		matcherStr = SpecialWordsProperties.replaceByVlaue(matcherStr);
-		return matcherStr;
-	}
+public class MDCodeReplace {
+    /**
+     * 处理包含这些单词的情况.
+     *
+     * @param matcherStr
+     * @return
+     */
+    public static String replaceContainSpecialWords(String matcherStr) {
+        matcherStr = ContainSpecialWordsProperties
+                .repalceByProperties(matcherStr);
+        return matcherStr;
+    }
+
+    /**
+     * 替换特殊字符.
+     *
+     * @param matcherStr.
+     * @return
+     */
+    public static String replaceSpecialChars(String matcherStr) {
+        matcherStr = matcherStr.replace("=", "等于");
+        matcherStr = matcherStr.replace("-", "杠");
+        matcherStr = matcherStr.replace("&", "单与符号");
+        matcherStr = matcherStr.replace("?", "问号");
+        matcherStr = matcherStr.replace("*", "星号");
+        matcherStr = matcherStr.replace("/", "斜杠");
+        matcherStr = matcherStr.replace("\\", "反斜杠");
+        matcherStr = matcherStr.replace(":", "冒号");
+        matcherStr = matcherStr.replace("$", "美元");
+        matcherStr = matcherStr.replace("#", "井号");
+        matcherStr = matcherStr.replace(".", "点");
+        matcherStr = matcherStr.replace("|", "竖杠");
+        matcherStr = matcherStr.replace("!", "感叹号");
+        matcherStr = matcherStr.replace("~", "波浪号");
+        matcherStr = matcherStr.replace("^", "脱字符");
+//		matcherStr = matcherStr.replace(",", "逗号");
+        return matcherStr;
+    }
+
+    /**
+     * 处理匹配特定正则表达式的情况.
+     *
+     * @param matcherStr
+     * @return
+     */
+    public static String replaceMatcher(String matcherStr) {
+        // HTML代码
+        Pattern htmlTagPattern = Pattern.compile("<(.+?)/?>");
+        Matcher htmlTagMatcher = htmlTagPattern.matcher(matcherStr);
+        // 如果是负数的情况
+//		if (matcherStr.matches("-\\d+"))
+        if (matcherStr.matches(RegexMath.negativeNumber)) {
+            // System.out.println("匹配到负数: " + matcherStr);
+            matcherStr = matcherStr.replace("-", "负");
+        }
+        // 朗读全大写字符串
+        matcherStr = replaceStringAllUpperCase(matcherStr);
+
+        // 如果是html标签
+        if (htmlTagMatcher.matches()) {
+            // 移除html标签的`<`和`/?>`
+            matcherStr = htmlTagMatcher.replaceAll("$1");
+        }
+        return matcherStr;
+    }
+
+    /**
+     * 朗读全大写的字符串.
+     *
+     * @param matcherStr 全部大写的字符串.
+     * @return 各个大写分开的字符串.
+     */
+    private static String replaceStringAllUpperCase(String matcherStr) {
+        Pattern pattern = Pattern.compile("[A-Z]+");
+        Matcher matcher = pattern.matcher(matcherStr);
+        if (matcher.matches()) {
+            StringBuffer sbBuffer = new StringBuffer(matcherStr.length() * 2);
+            for (int i = 0, length = matcherStr.length(); i < length; i++) {
+                sbBuffer.append(matcherStr.charAt(i) + " ");
+            }
+            return sbBuffer.toString();
+        }
+        return matcherStr;
+    }
+
+    /**
+     * 替换容易读错的单词.
+     *
+     * @param matcherStr 容易读错的单词.
+     * @return
+     */
+    public static String replaceSpecialWords(String matcherStr) {
+        matcherStr = SpecialWordsProperties.replaceByVlaue(matcherStr);
+        return matcherStr;
+    }
 }
--- src/replace/md/table/MDTableReplace.java
+++ src/replace/md/table/MDTableReplace.java
@@ -1,128 +1,123 @@
 package replace.md.table;
 
 //导入正则表达式
-import static regex.RegexJava.javaMethedRegex;
-import static regex.RegexMarkdown.MDCodeInLineInMDTableColsRegex;
-import static regex.RegexMarkdown.MDTableRegex;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 import clipboard.util.SysClipboardUtil;
 import replace.MDCodeReplace;
 
-public class MDTableReplace
-{
-	/**
-	 * 朗读Markdown表格,当表格中有方法声明时.
-	 * 
-	 * @param line
-	 *            markdown文档中的内容字符串.
-	 * @return
-	 */
-	public static String MDTableSpeech(String line)
-	{
-		Pattern pattern = Pattern.compile(MDTableRegex);
-		Matcher matcher = pattern.matcher(line);
-		StringBuffer sb = new StringBuffer();
-		String tableTitle;
-		// String tableAlign;
-		String tableBody;
-		String table;
-		while (matcher.find())
-		{
-			// 获取匹配到的一个分组
-			tableTitle = matcher.group(1);
-			tableBody = matcher.group(2);
-			tableBody = MDTableCodeInLineRepalce(tableBody);
-			table = tableTitle + tableBody;
-			matcher.appendReplacement(sb, table);
-		}
-		matcher.appendTail(sb);
-		return sb.toString();
-	}
-	/**
-	 * 替换表格中的行内代码.
-	 * 
-	 * @param tableBody
-	 * @return
-	 */
-	public static String MDTableCodeInLineRepalce(String tableBody)
-	{
-		// Pattern pattern = Pattern.compile(MDCodeInLineRegex);
-		Pattern pattern = Pattern.compile(MDCodeInLineInMDTableColsRegex);
-		Matcher matcher = pattern.matcher(tableBody);
-		StringBuffer sb = new StringBuffer();
-		String group1;
-		while (matcher.find())
-		{
-			// 删除反引号
-			group1 = matcher.group(1);
-			// 朗读Java方法
-			group1 = replaceJavaMethod(group1);
-			// 替换特殊字符
-			group1 = "|" + MDCodeReplace.replaceSpecialChars(group1) + "|";
-			// 替换原来匹配的文本
-			matcher.appendReplacement(sb, group1);
-		}
-		// 添加后面没有匹配的文本
-		matcher.appendTail(sb);
-		// // 删除多余的反引号
-		// String result = sb.toString();
-		// result = result.replaceAll(RegexMarkdown.MDCodeInLineRegex, "$1");
-		// return sb.toString();
-		return sb.toString();
-	}
-	/**
-	 * java方法的朗读规则.
-	 * 
-	 * @param matcherStr
-	 *            java方法定义字符串.
-	 * @return 朗读后的java方法.
-	 */
-	private static String replaceJavaMethod(String matcherStr)
-	{
-		Pattern pattern = Pattern.compile(javaMethedRegex);
-		Matcher matcher = pattern.matcher(matcherStr);
-		String group1;
-		String group2;
-		String group3;
-		if (matcher.matches())
-		{
-			// 获取匹配到的一个分组
-			group1 = matcher.group(1);
-			group2 = matcher.group(2);
-			group3 = matcher.group(3);
-			// System.out.println("捕获到表格:开始处理----------");
-			// System.out.println(" group1-->" + group1);
-			// System.out.println(" group2-->" + group2);
-			// System.out.println(" group3-->" + group3);
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-			matcherStr = group2 + " 方法 ";
-			if (!group1.equals(group2))
-			{
-				group1 = group1.replace("<?>", "泛型");
-				group1 = group1.replace("[]", "数组");
-				matcherStr = matcherStr + " 该方法定义 " + group1;
-			}
-			if (!"".equals(group3))
-			{
-				group3 = group3.replace("...", "变长参数");
-				group3 = group3.replace("<?>", "泛型");
-				group3 = group3.replace("[]", "数组");
-				group3 = group3.replace(",", "逗号");
-				matcherStr = matcherStr + " 该方法参数列表 " + group3;
-			} else
-			{
-				matcherStr = matcherStr + " 该方法是无参方法 ";
-			}
-			matcherStr = matcherStr + " 该方法功能 ";
-			// System.out.println(" 替换结果:" + matcherStr);
-			// System.out.println("捕获到表格:处理结束----------");
-		}
-		return matcherStr;
-	}
-	public static void main(String[] args)
-	{
-		String input = SysClipboardUtil.getSysClipboardText();
-		System.out.println(MDTableSpeech(input));
-	}
+import static regex.RegexJava.javaMethedRegex;
+import static regex.RegexMarkdown.MDCodeInLineInMDTableColsRegex;
+import static regex.RegexMarkdown.MDTableRegex;
+
+public class MDTableReplace {
+    /**
+     * 朗读Markdown表格,当表格中有方法声明时.
+     *
+     * @param line markdown文档中的内容字符串.
+     * @return
+     */
+    public static String MDTableSpeech(String line) {
+        Pattern pattern = Pattern.compile(MDTableRegex);
+        Matcher matcher = pattern.matcher(line);
+        StringBuffer sb = new StringBuffer();
+        String tableTitle;
+        // String tableAlign;
+        String tableBody;
+        String table;
+        while (matcher.find()) {
+            // 获取匹配到的一个分组
+            tableTitle = matcher.group(1);
+            tableBody = matcher.group(2);
+            tableBody = MDTableCodeInLineRepalce(tableBody);
+            table = tableTitle + tableBody;
+            //转义字符,$在这里表示捕获组,\\$表示这个字符本身.
+            table = table.replace("$", "\\$");
+            matcher.appendReplacement(sb, table);
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    /**
+     * 替换表格中的行内代码.
+     *
+     * @param tableBody
+     * @return
+     */
+    public static String MDTableCodeInLineRepalce(String tableBody) {
+        // Pattern pattern = Pattern.compile(MDCodeInLineRegex);
+        Pattern pattern = Pattern.compile(MDCodeInLineInMDTableColsRegex);
+        Matcher matcher = pattern.matcher(tableBody);
+        StringBuffer sb = new StringBuffer();
+        String group1;
+        while (matcher.find()) {
+            // 删除反引号
+            group1 = matcher.group(1);
+            // 朗读Java方法
+            group1 = replaceJavaMethod(group1);
+            // 替换特殊字符
+            group1 = "|" + MDCodeReplace.replaceSpecialChars(group1) + "|";
+            // 替换原来匹配的文本
+            matcher.appendReplacement(sb, group1);
+        }
+        // 添加后面没有匹配的文本
+        matcher.appendTail(sb);
+        // // 删除多余的反引号
+        // String result = sb.toString();
+        // result = result.replaceAll(RegexMarkdown.MDCodeInLineRegex, "$1");
+        // return sb.toString();
+        return sb.toString();
+    }
+
+    /**
+     * java方法的朗读规则.
+     *
+     * @param matcherStr java方法定义字符串.
+     * @return 朗读后的java方法.
+     */
+    private static String replaceJavaMethod(String matcherStr) {
+        Pattern pattern = Pattern.compile(javaMethedRegex);
+        Matcher matcher = pattern.matcher(matcherStr);
+        String group1;
+        String group2;
+        String group3;
+        if (matcher.matches()) {
+            // 获取匹配到的一个分组
+            group1 = matcher.group(1);
+            group2 = matcher.group(2);
+            group3 = matcher.group(3);
+            // System.out.println("捕获到表格:开始处理----------");
+            // System.out.println(" group1-->" + group1);
+            // System.out.println(" group2-->" + group2);
+            // System.out.println(" group3-->" + group3);
+
+            matcherStr = group2 + " 方法 ";
+            if (!group1.equals(group2)) {
+                group1 = group1.replace("<?>", "泛型");
+                group1 = group1.replace("[]", "数组");
+                matcherStr = matcherStr + " 该方法定义 " + group1;
+            }
+            if (!"".equals(group3)) {
+                group3 = group3.replace("...", "变长参数");
+                group3 = group3.replace("<?>", "泛型");
+                group3 = group3.replace("[]", "数组");
+                group3 = group3.replace(",", "逗号");
+                matcherStr = matcherStr + " 该方法参数列表 " + group3;
+            } else {
+                matcherStr = matcherStr + " 该方法是无参方法 ";
+            }
+            matcherStr = matcherStr + " 该方法功能 ";
+            // System.out.println(" 替换结果:" + matcherStr);
+            // System.out.println("捕获到表格:处理结束----------");
+        }
+        return matcherStr;
+    }
+
+    public static void main(String[] args) {
+        String input = SysClipboardUtil.getSysClipboardText();
+        System.out.println(MDTableSpeech(input));
+    }
 }
