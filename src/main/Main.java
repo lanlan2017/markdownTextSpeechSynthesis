d611740401b8ebc755bc827c4df7bd0471d27b98
lanlan2017 <18251956727@163.com>
2019-08-16 14:10:49 +0800

    修改一些方法的访问权限

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,11 @@
 package main;
 
 import clipboard.util.SysClipboardUtil;
-import replace.RemoveHtmlTags;
-import replace.RemoveMarkDownTags;
-import replace.ReplaceEnglishString;
-import replace.ReplaceSpaceInChineses;
 import system.call.cmd.Command;
-import tools.io.markdown.reader.MyMarkdownReader;
-import tools.io.properties.ChinesePolysyllabicWordsProperties;
-import tools.io.properties.ContainSpecialWordsProperties;
-import tools.io.properties.PerorationProperties;
-import tools.io.properties.SpeechSynthesisProperties;
-import tools.io.writer.MyStringWriter;
 
 import java.io.File;
-import java.io.FilenameFilter;
 
 public class Main {
-    static String fileName = null;
-    // 一次支持合成的最大字符数量
-    static int maxSize = 4000;
 
     public static void main(String[] args) {
         switch (args.length) {
@@ -50,7 +36,7 @@ public class Main {
     }
 
     /**
-     *
+     * 没有参数的情况.
      */
     private static void noArgs() {
         // 从剪贴板获取文件路径
@@ -60,122 +46,13 @@ public class Main {
             // 如果是文件的地址的话
             if (file.isFile()) {
                 //合成一个文件
-                xunfeiOneFile(path);
+                XunFeiTools.xunfeiOneFile(path);
             } else if (file.isDirectory()) {
-                xunfeiDir(file);
+                XunFeiTools.xunfeiDir(file);
             }
         } else {
             // 直接合成文本
-            xunfeiByContext(path);
-        }
-    }
-
-
-    /**
-     * 直接合成文本.
-     *
-     * @param context 文本内容.
-     */
-    private static void xunfeiByContext(String context) {
-        // 为了阅读方便
-        String input = context;
-        // 处理输入的文本
-        input = inputProcessing(input);
-        System.out.println("---------------- 直接合成 --------------------");
-        System.out.println(input);
-        System.out.println("---------------- 直接合成 --------------------");
-        fileName = System.getProperty("user.dir") + File.separator
-                + "直接合成.pcm";
-        // 合成所有的文本
-        XunFeiTools.xunfeiAll(input, maxSize, fileName);
-    }
-
-    /**
-     * 处理用户熟人的Markdown文本.
-     *
-     * @return
-     */
-    public static String inputProcessing(String input) {
-        System.out.println(
-                "######################################## 讯飞语音合成系统 ########################################");
-        // 移除中文之间的一个或多个空格
-        input = ReplaceSpaceInChineses.replaceSpaceInChineses(input);
-        // 移除类似`<center><strong>表19.3input标签的属性</strong></center>`这样的标签
-        input = RemoveHtmlTags.removeHtmlDoubleTags(input);
-        // 替换多音词,如重载,长度,机器可能会读错
-        input = ChinesePolysyllabicWordsProperties.replaceByValue(input);
-        // 替换容易读错的单词
-        input = ContainSpecialWordsProperties.repalceByProperties(input);
-        // 移除markdown标记
-        input = RemoveMarkDownTags.replaceMD(input);
-        // 拆分java驼峰命名法
-        input = ReplaceEnglishString.replaceEnglish(input);
-        //
-        input = input.replaceAll("(?m)[ ]+$", "");
-        return input;
-    }
-
-    /**
-     * 生成音频文件的绝对路径.
-     *
-     * @param path hexo博客中markdown文件的路径.
-     * @return 生成的音频文件的路径.
-     */
-    public static String filePath(String path) {
-        // 存放语音文件的根目录
-        // String fileName = "G:\\Desktop\\语音合成";
-        String fileName = SpeechSynthesisProperties.getRootDir();
-        // markdown文件相对于博客文章目录的相对路径
-        String relativePath = path.substring(
-                path.indexOf("source\\_posts") + "source\\_posts".length());
-        // 语音文件的根路径拼接上相对路径得到语音文件的绝对路径
-        fileName = fileName + relativePath.replace(".md", ".pcm");
-        return fileName;
-    }
-
-    /**
-     * 合成一个文件.
-     *
-     * @param path 文件的路径.
-     */
-    public static void xunfeiOneFile(String path) {
-        // 根据markdown文件的路径生成音频文件的路径
-        fileName = filePath(path);
-        // 从文件中读取markdown文本
-        String input = MyMarkdownReader.readerMyMarkdownFile(path)
-                .toString();
-        // 处理输入的文本
-        input = inputProcessing(input);
-        // 添加结束语
-        input = input + PerorationProperties.getPeroration();
-        // 合成所有的文本
-        XunFeiTools.xunfeiAll(input, maxSize, fileName);
-        // 7.给出提示
-        MyStringWriter.writerString(input);
-    }
-
-    /**
-     * 合成整个目录.
-     *
-     * @param file
-     */
-    public static void xunfeiDir(File file) {
-        System.out.println("-------------------------合成整个目录-----------------");
-        // 获取该目录下的所有markdown文件,或者子目录
-        File[] fileDirList = file.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".md") || new File(dir, name).isDirectory();
-            }
-        });
-        for (int i = 0; i < fileDirList.length; i++) {
-            if (fileDirList[i].isFile()) {
-                System.out.println("文件:" + file.getAbsolutePath());
-                xunfeiOneFile(fileDirList[i].getAbsolutePath());
-            } else if (fileDirList[i].isDirectory()) {
-                xunfeiDir(fileDirList[i]);
-            }
-
+            XunFeiTools.xunfeiByContext(path);
         }
     }
 }
--- src/main/XunFeiTools.java
+++ src/main/XunFeiTools.java
@@ -1,14 +1,36 @@
 package main;
 
+import replace.RemoveHtmlTags;
+import replace.RemoveMarkDownTags;
+import replace.ReplaceEnglishString;
+import replace.ReplaceSpaceInChineses;
 import system.call.cmd.Command;
+import tools.io.markdown.reader.MyMarkdownReader;
+import tools.io.properties.ChinesePolysyllabicWordsProperties;
+import tools.io.properties.ContainSpecialWordsProperties;
+import tools.io.properties.PerorationProperties;
+import tools.io.properties.SpeechSynthesisProperties;
+import tools.io.writer.MyStringWriter;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class XunFeiTools {
+    /**
+     * 等待标记.如果为true则主线程等待.
+     */
     static boolean wait = true;
+    /**
+     * 部分临时文件路径列表.
+     */
     static ArrayList<String> fileNameList = null;
+    /**
+     * 传入的文件名称.
+     */
+    static String fileName = null;
+    // 一次支持合成的最大字符数量
+    static int maxSize = 4000;
 
     /**
      * 生成部分语音的路径.
@@ -17,7 +39,7 @@ public class XunFeiTools {
      * @param count    计数器
      * @return 部分音频的路径.
      */
-    public static String fileNamePart(String fileName, int count) {
+    private static String fileNamePart(String fileName, int count) {
         String fileNamePart;
         // 第一个部分使用原来的文件名
         fileNamePart = fileName.substring(0, fileName.lastIndexOf(".pcm"))
@@ -30,11 +52,11 @@ public class XunFeiTools {
      *
      * @return 保存音频的文件列表.
      */
-    public static ArrayList<String> getFileNameList() {
+    static ArrayList<String> getFileNameList() {
         return fileNameList;
     }
 
-    public static void setFileNameList(ArrayList<String> fileNameList) {
+    static void setFileNameList(ArrayList<String> fileNameList) {
         XunFeiTools.fileNameList = fileNameList;
     }
 
@@ -43,7 +65,7 @@ public class XunFeiTools {
      *
      * @param filePath 音频文件的路径.
      */
-    public static void openFileUseAudition(String filePath) {
+    private static void openFileUseAudition(String filePath) {
         String audition = "F:\\软件\\安装包_音频处理\\Adobe\" \"Audition\" \"CS6\\Adobe\" \"Audition\" \"CS6.exe ";
         // String filePath = "G:\\Desktop\\语音合成\\疯狂Java讲义第三版\\第13章\\13.2.4 DDL语句
         // 2.修改表结构的语法.pcm";
@@ -54,7 +76,7 @@ public class XunFeiTools {
     /**
      * 阻塞线程.
      */
-    public static void waitFor() {
+    private static void waitFor() {
         while (wait) {
             try {
                 Thread.sleep(1000);
@@ -71,7 +93,7 @@ public class XunFeiTools {
      * @param maxSize  讯飞SDK每次最多接收的字数上限,我测试了,这个设为4000没有问题.
      * @param fileName 要合成生成的文件.
      */
-    public static void xunfeiAll(String input, int maxSize, String fileName) {
+    private static void xunfeiAll(String input, int maxSize, String fileName) {
         // System.out.println("字符串总长度:" + input.length());
         // 整个字符串的长度
         int inputLength = input.length();
@@ -120,10 +142,10 @@ public class XunFeiTools {
     /**
      * 合成一部分.
      *
-     * @param input
-     * @param fileNamePart
+     * @param input 输入文本.
+     * @param fileNamePart 部分文件路径.
      */
-    public static void xunfeiOnes(String input, String fileNamePart) {
+    private static void xunfeiOnes(String input, String fileNamePart) {
         new XunFei(input, fileNamePart).xunfei();
         wait = true;
         // 等待上次执行完毕
@@ -136,13 +158,13 @@ public class XunFeiTools {
      * @param targetFilePath     目标文件路径名称字符串.
      * @param sourceFilePathList 存放源文件的路径名称字符串的ArrayList集合.
      */
-    public static void merge2TargetFileDeleteSourceFile(String targetFilePath,
-                                                        ArrayList<String> sourceFilePathList) {
+    private static void merge2TargetFileDeleteSourceFile(String targetFilePath,
+                                                         ArrayList<String> sourceFilePathList) {
         // 如果ArrayList中有东西
         if (sourceFilePathList.size() > 0) {
             BufferedInputStream in;
             try (BufferedOutputStream out = new BufferedOutputStream(
-                    new FileOutputStream(new File(targetFilePath)));) {
+                    new FileOutputStream(new File(targetFilePath)))) {
 
                 System.out.println("源文件列表:");
                 for (Iterator<String> iterator = sourceFilePathList
@@ -175,8 +197,8 @@ public class XunFeiTools {
         }
     }
 
-    public static void mergeAndOpenFile(String targetFilePath,
-                                        ArrayList<String> sourceFilePathList) {
+    private static void mergeAndOpenFile(String targetFilePath,
+                                         ArrayList<String> sourceFilePathList) {
         // 合并list中的源文件到目标文件,并删除源文件
         merge2TargetFileDeleteSourceFile(targetFilePath, sourceFilePathList);
         // 清空List
@@ -185,5 +207,106 @@ public class XunFeiTools {
         System.out.println("打开合并后的文件:" + targetFilePath);
         XunFeiTools.openFileUseAudition(targetFilePath);
     }
+    /**
+     * 生成音频文件的绝对路径.
+     *
+     * @param path hexo博客中markdown文件的路径.
+     * @return 生成的音频文件的路径.
+     */
+    private static String filePath(String path) {
+        // 存放语音文件的根目录
+        // String fileName = "G:\\Desktop\\语音合成";
+        String fileName = SpeechSynthesisProperties.getRootDir();
+        // markdown文件相对于博客文章目录的相对路径
+        String relativePath = path.substring(
+                path.indexOf("source\\_posts") + "source\\_posts".length());
+        // 语音文件的根路径拼接上相对路径得到语音文件的绝对路径
+        fileName = fileName + relativePath.replace(".md", ".pcm");
+        return fileName;
+    }
+    /**
+     * 合成一个文件.
+     *
+     * @param path 文件的路径.
+     */
+    public static void xunfeiOneFile(String path) {
+        // 根据markdown文件的路径生成音频文件的路径
+        fileName = filePath(path);
+        // 从文件中读取markdown文本
+        String input = MyMarkdownReader.readerMyMarkdownFile(path)
+                .toString();
+        // 处理输入的文本
+        input = inputProcessing(input);
+        // 添加结束语
+        input = input + PerorationProperties.getPeroration();
+        // 合成所有的文本
+        XunFeiTools.xunfeiAll(input, maxSize, fileName);
+        // 7.给出提示
+        MyStringWriter.writerString(input);
+    }
+    /**
+     * 合成整个目录.
+     *
+     * @param file
+     */
+    public static void xunfeiDir(File file) {
+        System.out.println("-------------------------合成整个目录-----------------");
+        // 获取该目录下的所有markdown文件,或者子目录
+        File[] fileDirList = file.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".md") || new File(dir, name).isDirectory();
+            }
+        });
+        for (int i = 0; i < fileDirList.length; i++) {
+            if (fileDirList[i].isFile()) {
+                System.out.println("文件:" + file.getAbsolutePath());
+                xunfeiOneFile(fileDirList[i].getAbsolutePath());
+            } else if (fileDirList[i].isDirectory()) {
+                xunfeiDir(fileDirList[i]);
+            }
 
+        }
+    }
+    /**
+     * 处理用户输入的Markdown文本.
+     *
+     * @return 移除Markdown标记之后的文本.
+     */
+    private static String inputProcessing(String input) {
+        System.out.println(
+                "######################################## 讯飞语音合成系统 ########################################");
+        // 移除中文之间的一个或多个空格
+        input = ReplaceSpaceInChineses.replaceSpaceInChineses(input);
+        // 移除类似`<center><strong>表19.3input标签的属性</strong></center>`这样的标签
+        input = RemoveHtmlTags.removeHtmlDoubleTags(input);
+        // 替换多音词,如重载,长度,机器可能会读错
+        input = ChinesePolysyllabicWordsProperties.replaceByValue(input);
+        // 替换容易读错的单词
+        input = ContainSpecialWordsProperties.repalceByProperties(input);
+        // 移除markdown标记
+        input = RemoveMarkDownTags.replaceMD(input);
+        // 拆分java驼峰命名法
+        input = ReplaceEnglishString.replaceEnglish(input);
+        //
+        input = input.replaceAll("(?m)[ ]+$", "");
+        return input;
+    }
+    /**
+     * 直接合成文本.
+     *
+     * @param context 文本内容.
+     */
+    public static void xunfeiByContext(String context) {
+        String input = context;
+        // 处理输入的文本
+        input = XunFeiTools.inputProcessing(input);
+        System.out.println("---------------- 直接合成 --------------------");
+        System.out.println(input);
+        System.out.println("---------------- 直接合成 --------------------");
+        fileName = System.getProperty("user.dir") + File.separator
+                + "直接合成.pcm";
+        // 合成所有的文本
+        XunFeiTools.xunfeiAll(input, maxSize, fileName);
+    }
 }
